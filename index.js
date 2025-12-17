bca5248923d65341c38c253859028f8f8000301e
Joshua <jngmerteyx@gmail.com>
2025-12-17 12:51:42 +0000

    Initial Node.js app for Buddy CI/CD

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Buddy CI/CD deployed this app successfully 🚀",
+    timestamp: new Date(),
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
